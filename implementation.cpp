35c7d235608d3d5cf8a89404aaabaaaa7e6e720c
Ziad Morsi <24-101044@students.eui.edu.eg>
2025-12-30 22:33:18 -0800

    chages to dequeu function

--- implementation.cpp
+++ implementation.cpp
@@ -278,7 +278,7 @@ void ratingOrder ::enqueue(article* toAdd){
 article ratingOrder ::dequeue(){
     if (isEmpty())
     {
-        return;
+        return article();
     }
     article* toDelete = head;
     head = head ->next;
